f36c395bd59efb4ba86859dd9e4a225f3db0cc6c
Dave Winniford <dwinniford@gmail.com>
2020-02-12 22:12:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -79,7 +79,7 @@ class Dog
     sql = <<-SQL
       SELECT *
       FROM dogs 
-      WHERE name = ? , breed = ?
+      WHERE name = ? AND breed = ?
     SQL
     row = DB[:conn].execute(sql, name, breed)[0]
     if !row.empty?
